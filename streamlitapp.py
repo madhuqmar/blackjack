dacd9adfdf5714ae29dddef1f5b716850295f586
Madhumitha Kumar <madhu@iMac.local>
2023-10-13 10:33:11 -0400

    cache command

--- streamlitapp.py
+++ streamlitapp.py
@@ -7,7 +7,7 @@ number_of_decks = 6
 blackjack_multiplier = 1.5
 
 
-@st.cache_data(allow_output_mutation=True, suppress_st_warning=True)
+@st.cache(allow_output_mutation=True, suppress_st_warning=True)
 def start_game():
     game_deck = Deck(number_of_decks)
     dealer = Dealer()
