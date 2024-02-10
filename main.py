5b49133e4ab66eb69ddd391079d862d484ecb1d8
Madhumitha Kumar <madhu@iMac.local>
2024-02-10 12:39:29 -0500

    metrics fixing

--- main.py
+++ main.py
@@ -10,54 +10,59 @@ st.subheader("You get to start with $1000")
 
 st.divider()
 
-bet_amounts = [0, 50, 100, 200]
-selected_bet_amount = st.selectbox("Select your bet amount", bet_amounts)
-
-# Initialize session state variables
-if 'last_outcome' not in st.session_state:
-    st.session_state.last_outcome = 'awaiting game'
-
-if st.session_state.last_outcome == "Win":
-    st.session_state.last_outcome_score = 1
-elif st.session_state.last_outcome == "Loss":
-    st.session_state.last_outcome_score = -1
-elif st.session_state.last_outcome == "Push":
-    st.session_state.last_outcome_score = 0
-elif st.session_state.last_outcome == "awaiting game":
-    st.session_state.last_outcome_score = 0
-
-if 'current_amount' not in st.session_state:  # if game not started
-    st.session_state.current_amount = 1000
+### TRACKING BET AMOUNTS ###
 
 if 'bet_amount' not in st.session_state:
     st.session_state.bet_amount = 0
-else:
-    st.session_state.bet_amount = selected_bet_amount
+
+bet_amounts = [0, 50, 100, 200]
+
+selected_bet_amount = st.selectbox("Select your bet amount", bet_amounts, index=bet_amounts.index(st.session_state.bet_amount))
+
+st.session_state.bet_amount = selected_bet_amount
 
 if 'last_bet_amount' not in st.session_state:
     st.session_state.last_bet_amount = 0
 
-@st.cache_resource()
-def start_game():
-    game_deck = Deck(number_of_decks)
-    dealer = Dealer()
-    player = Player()
-    game_play = GamePlay(player, dealer, game_deck, blackjack_multiplier, selected_bet_amount)
-    return game_deck, dealer, player, game_play
+
+### TRACKING PLAYER GAME OUTCOMES ###
+
+if 'last_outcome' not in st.session_state:
+    st.session_state.last_outcome = 'awaiting game'
+
+if 'last_outcome_score' not in st.session_state:
+    st.session_state.last_outcome_score = 0
+
+
+### TRACKING PLAYER POCKETS ###
+
+if 'current_amount' not in st.session_state:  # if game not started
+    st.session_state.current_amount = 1000
+
+
 
 def main():
-    game_deck, dealer, player, game_play = start_game()
+
+    st.write(st.session_state.bet_amount)
+
+    @st.cache_resource()
+    def start_game(bet_amount):
+        game_deck = Deck(number_of_decks)
+        dealer = Dealer()
+        player = Player()
+        game_play = GamePlay(player, dealer, game_deck, blackjack_multiplier, bet_amount)
+        return game_deck, dealer, player, game_play
+        st.write(st.session_state.bet_amount)
+
+    game_deck, dealer, player, game_play = start_game(st.session_state.bet_amount)
 
     if st.button('Play with my bets'):
         game_play.deal_in()
-        st.session_state.current_amount -= selected_bet_amount
-        if 'last_outcome' in st.session_state:
-            if st.session_state.last_outcome_score == 1:
-                st.session_state.current_amount += st.session_state.last_bet_amount * blackjack_multiplier
-            elif st.session_state.last_outcome_score == -1:
-                st.session_state.current_amount -= st.session_state.last_bet_amount
+        st.session_state.current_amount -= st.session_state.bet_amount
+        # st.session_state.game_processed = False
     
     col1, col2, col3 = st.columns(3)
+
     col1.metric(label="Pockets", value=f"${st.session_state.current_amount:,.2f}", delta=0)
     col2.metric(label="Current Bet", value=f"${st.session_state.bet_amount:,.2f}", delta=st.session_state.bet_amount - st.session_state.last_bet_amount)
     col3.metric(label="Last Outcome", value=st.session_state.last_outcome, delta=st.session_state.last_outcome_score)
@@ -97,8 +102,29 @@ def main():
 
     #Update user metrics
     st.session_state.last_outcome = game_play.player_win
-    st.session_state.last_bet_amount = game_play.bet_amount
+    st.session_state.last_bet_amount = game_play.player_win_amount
 
+    if st.session_state.last_outcome == "Win":
+        st.session_state.last_outcome_score = 1
+    elif st.session_state.last_outcome == "Loss":
+        st.session_state.last_outcome_score = -1
+    elif st.session_state.last_outcome == "Push":
+        st.session_state.last_outcome_score = 2
+    elif st.session_state.last_outcome == "awaiting game":
+        st.session_state.last_outcome_score = 0
+
+
+    st.write(st.session_state.last_outcome)
+    st.write(st.session_state.last_outcome_score)
+    st.write(st.session_state.last_bet_amount)
+
+
+
+    # Only update current amount if the current game hasn't been processed yet and the outcome is a win
+    if st.session_state.last_outcome_score == 1:
+        st.session_state.current_amount += game_play.player_win_amount
+    elif st.session_state.last_outcome_score == 2:
+         st.session_state.current_amount += game_play.bet_amount
 
     player_stats.write(player)
     player_images.image([Image.open(card.image_location)
